4b1a1f007a56cc5e77fc9c24fd1d0d013c5db993
savoptik <sav70792@gmail.com>
2018-09-21 16:15:22 +0300

    Initial Commit

---
+++ hopfield network/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hopfield network
+//
+//  Created by Артём Семёнов on 21/09/2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
